4101ada42b7c24a691d994d98117fc04884d99ad
Lesley Moore <ldmoore15@gmail.com>
2021-05-08 14:57:23 -0400

    modified:   script.js

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ var search = function(e){
 }
 
 console.log(citysearch);
-var api5day = "https://api.openweathermap.org/data/2.5/forecast?q=" + citysearch + "&appid=bb55c27a72b025f34bdda7148dc83953";
-var currentapi = "https://api.openweathermap.org/data/2.5/weather?q=" + citysearch + "&appid={bb55c27a72b025f34bdda7148dc83953}";
+var api5day = ;
+var currentapi = ;
 
 console.log(api5day);
 console.log(currentapi);
